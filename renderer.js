c1ea126d319485962cc10f66880679f81fed96a8
Lester Fong <lester.fong@switchconnect.com.au>
2024-07-22 22:00:01 +0800

    game updates

--- renderer.js
+++ renderer.js
@@ -4,16 +4,33 @@ let animationInProgress = false;
 const dices = document.querySelectorAll(".dice");
 const rollBtn = document.getElementById('roll');
 const shuffleBtn = document.getElementById('shuffle');
-let animationDone = false;
+const audio = document.getElementById("gameAudio");
+const winnerAudio = document.getElementById("winnerAudio");
+let winningDices = [];
 
 document.addEventListener('DOMContentLoaded', () => {
     shuffleDice();
 });
 
+const shuffleDice = () => { 
+    dices.forEach(dice => {
+        const random = Math.floor(Math.random() * 7);
+        transformDice(random, dice);
+    })
+}
+
 const randomDice = () => {
     if (animationInProgress) {
         return;
     }
+
+    // reset audio timestamp
+    winnerAudio.currentTime = 0;
+    audio.currentTime = 0;
+    // play drum roll sound
+    audio.play();
+
+    winningDices = [];
     dices.forEach((dice, ndx) => {
         const random = Math.floor(Math.random() * 7);
         const duration = ndx == 0 ? 6 : ndx == 1 ? 7 : 9;
@@ -39,27 +56,39 @@ const randomDice = () => {
 const transformDice = (random, dice) => {
     switch (random) {
         case 1:
+            //RED
             dice.style.transform = 'rotateX(0deg) rotateY(0deg)';
+            winningDices.push("RED");
             break;
 
         case 6:
+            //BLUE
             dice.style.transform = 'rotateX(180deg) rotateY(0deg)';
+            winningDices.push("BLUE");
             break;
 
         case 2:
+            //GREEN
             dice.style.transform = 'rotateX(-90deg) rotateY(0deg)';
+            winningDices.push("GREEN");
             break;
 
         case 5:
+            //YELLOW
             dice.style.transform = 'rotateX(90deg) rotateY(0deg)';
+            winningDices.push("YELLOW");
             break;
 
         case 3:
+            //PINK
             dice.style.transform = 'rotateX(0deg) rotateY(90deg)';
+            winningDices.push("PINK");
             break;
 
         case 4:
+            //WHITE
             dice.style.transform = 'rotateX(0deg) rotateY(-90deg)';
+            winningDices.push("WHITE");
             break;
 
         default:
@@ -82,25 +111,57 @@ const rollDice = (random, dice, duration, ndx) => {
             animationInProgress = false;
             rollBtn.disabled = false;
             shuffleBtn.disabled = false;
-            
-            //swal here
-            Swal.fire({
-                title: 'You rolled a dice!',
-                text: `You rolled a ${random}!`,
-                icon: 'success',
-                confirmButtonText: 'Nice!'
-            });
+
+            // show winners
+            onDisplayWinners();
         } 
 
         dice.style.animation = 'none';
+       
     }, duration * 1000 + 1000);
-
+     
+   
 }
 
-const shuffleDice = () => { 
-    dices.forEach(dice => {
-        const random = Math.floor(Math.random() * 7);
-        transformDice(random, dice);
+const onDisplayWinners = () => {
+    winnerAudio.play();
+    Swal.fire({
+        title: 'Winning Dices',
+        html: `
+        <div class="flex">
+            ${winningDices.map(dice => `
+                <div class="dice">
+                    <div class="face-swal">
+                        <div class="swal-front" style="background: ${dice}"></div>
+                    </div>
+                </div>
+            `).join('')}
+        </div>
+        <div class="flex mt-2">
+            ${winningDices.map(dice => `
+                <h3 style="color: ${dice == 'WHITE'? 'black' : dice}">${dice}</h3>
+            `).join('')}
+        </div>
+        `,
+        background: "#fff url('./assets/images/trees.png')",
+        backdrop: `
+            rgba(0,0,123,0.4)
+            url("./assets/images/confetti.gif")
+            left top
+            repeat
+        `,
+        width: 1020,
+        allowOutsideClick: false,
+        showCloseButton: true,
+        showConfirmButton: false,
+        showCancelButton: true,
+        cancelButtonText: 'Play Again',
+    }).then((result) => {
+        if (result.isDismissed) {
+            winnerAudio.pause();
+            audio.pause();
+            
+        }
     })
 }
 
